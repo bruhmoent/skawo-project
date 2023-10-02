d8a285a6af48178c309300d1a33f003b8efe3d26
bruhmoent <kmega757@gmail.com>
2023-10-02 23:31:55 +0200

    Animations and trip information

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,6 +9,7 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    <script src="https://kit.fontawesome.com/8b6addd488.js" crossorigin="anonymous"></script>
 
     <!-- Styles -->
     <link rel="stylesheet" href="{{ asset('public_css/header.css') }}">
@@ -40,31 +41,89 @@
     <div id="middle">
         <h1 id="text-middle">What's our mission?</h1>
         <div id="middle-content">
-            <div id="infodiv">
-                <h5>Our offers right now:</h5>
-                <div id="offers-list"></div>
-            </div>
+        <div id="infodiv">
+        <h5>Our offers right now:</h5>
+        <hr class="faded-hr" style="width: 80%;">
+
+        <?php
+            $today = now()->toDateString();
+            $nextMonth = now()->addMonth()->toDateString();
+
+            $tripSchedules = DB::table('trip_schedules')
+                                ->where('start_date', '>=', $today)
+                                ->where('start_date', '<=', $nextMonth)
+                                ->get();
+
+            if ($tripSchedules->isEmpty()) 
+            {
+                echo '<div id="error-message">';
+                echo '<i class="fa-solid fa-triangle-exclamation fa-beat fa-3x" style="color: rgb(255, 255, 255);"></i><br>';
+                echo '<hr class="faded-hr" style="width: 50%;">';
+                echo '<div class="animated-text">Sorry! There aren\'t currently any active trips.</div>';
+                echo '</div>';
+            } 
+            else 
+            {
+                echo '<div id="offers-list-container">';
+                foreach ($tripSchedules as $tripSchedule)
+                {
+                    $tripName = $tripSchedule->description;
+                    $participantIds = json_decode($tripSchedule->participants);
+                    $participantCount = count($participantIds);
+                    $remainingSlots = max(0, 8 - $participantCount);
+
+                    echo '<div class="trip-offer">';
+                    echo '<h2>' . $tripName . '</h2><hr class="faded-hr" style="width: 60%;"> Participants: <hr class="faded-hr" style="width: 60%;"><br>';
+                    echo '<div class="person-container">';
+
+                    for ($i = 0; $i < $participantCount; $i++) 
+                    {
+                        echo '<i class="fa-solid fa-person participant"></i>';
+                    }
+                    
+                    for ($i = 0; $i < $remainingSlots; $i++) 
+                    {
+                        echo '<i class="fa-solid fa-person" style="color: gray;"></i>';
+                    }
+                    
+                    echo '</div>';
+                    echo '</div>';
+                }
+                echo '</div>';
+            }
+        ?>
+
+
+        </div>
+
         <div id="middle-center">
+                <div id="spin-wheel">
+             <div id="spinner">☸</div>
+             </div>
+
             <div id="text-container">
-            <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. ...
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. ...
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. ...
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. ...
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. ...
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. ...
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. ...
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. ...
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. ...
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. ...
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. ...
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. ...
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. ...
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. ...
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. ...
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. ...
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. ...
-            </p>
+                <h1>Welcome to Skawo: Your Gateway to Nordic Adventures!</h1>
+                <hr class="faded-hr" style="width: 80%;">
+                <p>
+                    Are you ready to embark on a journey of discovery through the stunning landscapes and vibrant cultures of Scandinavia? Look no further than Skawo - your digital passport to a world of Nordic wonders!
+                </p>
+
+                <p>
+                    Skawo is more than just a web app; it's an invitation to immerse yourself in the beauty and magic that Scandinavia has to offer. From the majestic fjords of Norway to the picturesque forests of Sweden, and the fascinating history of Denmark - Skawo is here to guide you on an unforgettable adventure.
+                </p>
+
+                <h2>Why Choose Skawo?</h2>
+
+                <ul>
+                <li>Explore the best destinations across Scandinavia with ease and convenience.</li>
+                <li>Discover hidden gems and lesser-known spots recommended by fellow adventurers.</li>
+                <li>Access expert travel tips and curated itineraries to make the most of your trip.</li>
+                <li>Connect with a community of like-minded explorers and share your experiences.</li>
+                </ul>
+
+                <p>
+                    Join us at Skawo and unlock a world of breathtaking landscapes, rich history, and the warm hospitality of the Nordic region. Your adventure begins here - let's embrace the beauty of Scandinavia together!
+                </p>
             </div>
             <button id="mid-but">
                 <p id="mid-but-txt">Register</p>
@@ -94,25 +153,28 @@
     </div>
 
     <script>
-        let li_els = document.querySelectorAll('ul li');
+        let slider = document.getElementById('slider');
+        let li_els = slider.querySelectorAll('li');
         let index = 0;
 
-        function show(increase) {
-            index = index + increase;
-            index = Math.min(Math.max(index, 0), li_els.length - 1);
-            li_els[index].scrollIntoView({
+        function show(increase) 
+        {
+        index = index + increase;
+        index = Math.min(Math.max(index, 0), li_els.length - 1);
+        li_els[index].scrollIntoView({
             behavior: 'smooth',
             block: 'center',
             inline: 'nearest'
-            });
+        });
         }
-
+    
         window.addEventListener("load", (event) => {
-        show(+1);
+            show(1);
         });
     </script>
 
     <?php
+    /*
         $participants = [1, 2, 3, 4];
         $json_participants = json_encode($participants);
 
@@ -196,6 +258,7 @@
         {
         // echo $participant . '<br>';
         }
+        */  
     ?>
 </body>
 </html>
